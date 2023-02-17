b274fe6859ef4aa061675c2c5679ee809a437e61
LibenS28 <libenseyoum28@gmail.com>
2023-02-16 23:24:59 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main () {
-	puts("\"Programming is like building a multilingual puzzle")
-		
-		retrun 0;}
+int main(void)
+	{
+		puts("\"Programming is like building a multilingual puzzle")
+		retrun 0;
+	}
