d7fd7a998edf8ebddea62cce24804c4331b0295a
LibenS28 <libenseyoum28@gmail.com>
2023-02-21 10:10:04 -0800

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,12 +4,20 @@
  * _abs - absolute value
  *
  * @n: parameter
+ * Return: Void.
  */
 int _abs(int n)
 {
 	if (n < 0)
 	{
-		return ( n * -1);
+		return (n * -1);
 	}
+	else if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
 	return (0);
+	}
 }
