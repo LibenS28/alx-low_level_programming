23a1df152ecfdec2e7ffcc142df0a6ad3b30b014
LibenS28 <libenseyoum28@gmail.com>
2023-02-21 07:49:42 -0800

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - check the code
+ * main - check the code.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
 void print_alphabet_x10(void)
 {
 int a = 0;
