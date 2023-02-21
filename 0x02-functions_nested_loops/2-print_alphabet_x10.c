cf3161b0135ed1904940acd054b62882ff4c1f80
LibenS28 <libenseyoum28@gmail.com>
2023-02-21 04:49:48 -0800

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - check the code
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
@@ -16,7 +16,7 @@ for (; a <= 122 ; a++)
 _putchar(a);
 }
 _putchar('\n');
-for (; b < 10 ; a++)
+for (; b < 10 ; b++)
 {
 print_alphabet_x10();
 }
