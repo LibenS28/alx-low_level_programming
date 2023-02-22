1889e3281d357879fdb60add9410a03a9c624705
LibenS28 <libenseyoum28@gmail.com>
2023-02-22 08:25:08 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - function to add
+ *@a: parameter
+ * Return: Always 0.
+ *@b: parameter
+ */
+
+int add(int a, int b)
+{
+	int a, b;
+	int add;
+
+	add = a + b;
+	printf("%d\n", add);
+	return (0);
+}
+
